76b3b08fa65698a802bf6183ac7ff6d4e5f46aab
marce1l <mikesmarcell@gmail.com>
2024-02-08 23:50:11 +0100

    First commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+/*
+Goal:
+    Create a Telegram bot that snipes crypto coins posted in another Telegram channel.
+        - Listens for the call
+        - Automatically swaps crypto to the posted coins
+        - Does basic due diligence about the coin (honeypot, fee, market cap etc..)
+        - Automatically realizes profit/loss once reaching a threshold
+
+APIs to be used:
+    - Etherscan
+    - Honeypot.is
+    - Telegram
+*/
+fn main() {
+    println!("Hello, world!");
+}
